e3fbbea7404c4b8f4778938229e541df7ca4cfe1
AntoninaVish <vishivtonya@gmail.com>
2023-02-25 18:18:15 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPISearchElementsByMultipleCondition
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            //собираем все экземпляры семейств окон без их типов, FamilyInstance -это экземпляр семейства
+            ElementCategoryFilter windowsCategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_Windows);// по категории
+            ElementClassFilter windowsInstancesFilter = new ElementClassFilter(typeof(FamilyInstance));// по экземпляру семейства
+
+            //LogicalAndFilter выполнять поиск элементов по двум условиям одновременно
+            LogicalAndFilter windowsFilter = new LogicalAndFilter(windowsCategoryFilter, windowsInstancesFilter);
+
+            //ищем окна в текущем документе
+            var windows = new FilteredElementCollector(doc)
+                .WherePasses(windowsFilter) //указываем путь где проходить(искать)
+                .Cast<FamilyInstance>() // Cast - забрасывает в список
+                .ToList();
+
+            TaskDialog.Show("Windows info" , windows.Count.ToString());//выводим инфор.окон, посчетать окна, в cтроку
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RevitAPISearchElementsByMultipleCondition")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RevitAPISearchElementsByMultipleCondition")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ca8d289e-9b4c-4fc5-bbeb-4e9163341793")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
